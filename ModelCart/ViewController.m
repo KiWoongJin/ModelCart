cdfeee8f18c39f0c4bbb154327d713d2bf53932b
KiWoongJin <jkw87first@naver.com>
2014-01-09 14:22:23 +0900

    ModelCart
    1.2.1 실

--- ModelCart/ViewController.m
+++ ModelCart/ViewController.m
@@ -7,17 +7,70 @@
 //
 
 #import "ViewController.h"
+#import "Product.h"
+#import "ProductCell.h"
+#import "CartDelegate.h"
+#import "Catalog.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>{
+    NSMutableArray *cartItems;
+}
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
 
+- (void)addItem:(id)sender{
+    
+    NSIndexPath *indexPath = [self.table indexPathForCell:sender];
+    Product *product = [[Catalog defaultCatalog]productAt:indexPath.row];
+    
+    [cartItems addObject:product];
+    
+    NSIndexSet *indexSet = [NSIndexSet indexSetWithIndex:1];
+    [self.table reloadSections:indexSet withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if(0 == section)
+        return [[Catalog defaultCatalog]numberOfProducts];
+    else
+        return [cartItems count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return (0 == section)?@"상품목록" : @"카트";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (0 == indexPath.section) {
+        ProductCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PRODUCT_CELL"];
+        Product *product = [[Catalog defaultCatalog]productAt:indexPath.row];
+        [cell setProductInfo:product];
+        
+        cell.delegate = self;
+        return cell;
+    }
+    else{
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CART_CELL"];
+        
+        Product *product = cartItems[indexPath.row];
+        cell.textLabel.text = product.name;
+        
+        return cell;
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    cartItems = [[NSMutableArray alloc]init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +78,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
